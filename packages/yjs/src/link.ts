238251908f4b5615e108d00041259886d9789ec4
Kyle Mathews <mathews.kyle@gmail.com>
2023-10-02 14:00:51 -0700

    The uuid package pulls in a bunch of extra stuff. crypto.getRandomValues is good enough

--- packages/yjs/src/link.ts
+++ packages/yjs/src/link.ts
@@ -1,8 +1,16 @@
 import type { AnyRouter } from "@trpc/server"
 import { Doc, YMapEvent, Map } from "yjs"
-import { v4 as uuidv4 } from "uuid"
 import { TRPCClientError, TRPCLink } from "@trpc/client"
 import { observable } from "@trpc/server/observable"
+
+function uuidv4() {
+  return `10000000-1000-4000-8000-100000000000`.replace(/[018]/g, (c) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+  )
+}
 
 export const link = <TRouter extends AnyRouter>({
   doc,
