af24cbf1450d024bf1a37c0934ec8cdf7032d011
Peter van Hardenberg <pvh@pvh.ca>
2023-11-09 19:53:43 -0800

    wip

--- examples/automerge/app/root.tsx
+++ examples/automerge/app/root.tsx
@@ -7,18 +7,17 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react"
-import { useEffect, useState } from "react"
-// import type { Socket } from "socket.io-client"
-// import io from "socket.io-client"
-import * as Y from "yjs"
-import { WebsocketProvider } from "y-websocket"
-import { YjsProvider } from "~/context"
 import styles from "./globals.css"
 import { cssBundleHref } from "@remix-run/css-bundle"
 import type { LinksFunction } from "@remix-run/node"
 import { createTRPCProxyClient, loggerLink, httpBatchLink } from "@trpc/client"
-import { link } from "trpc-yjs/link"
+import { link } from "trpc-automerge/link"
 import type { AppRouter } from "~/server/trpc"
+
+import { Repo } from "@automerge/automerge-repo"
+import { BrowserWebSocketClientAdapter } from "@automerge/automerge-repo-network-websocket"
+import { useDocument } from "@automerge/automerge-repo-react-hooks"
+
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: styles },
@@ -31,51 +30,29 @@ export const meta: MetaFunction = () => ({
   viewport: `width=device-width,initial-scale=1`,
 })
 
-function Loading({ doc, children }) {
+function Loading({ doc, children }: { doc: boolean, children: unknown }) {
   if (doc) {
     return children
   } else {
-    return <div></div>
+    return <div>Loading...</div>
   }
 }
-const doc = new Y.Doc()
+
+const repo = new Repo({network: [new BrowserWebSocketClientAdapter("wss://localhost:3030")]})
+const queueHandle = repo.create()
+
 const trpc = createTRPCProxyClient<AppRouter>({
   links: [
     loggerLink(),
     link({
-      doc,
+      repo, 
+      queueHandle,
     }),
   ],
 })
 
 export default function App() {
-  const [provider, setProvider] = useState<WebsocketProvider>()
-  const [syncedDoc, setDoc] = useState<Y.Doc>()
-
-  useEffect(() => {
-    console.log(`CREATING PROVIDER`)
-    const href = new URL(
-      `${location.protocol === `https:` ? `wss:` : `ws:`}//${
-        location.hostname
-      }:${location.port}`
-    ).href
-    const wsProvider = new WebsocketProvider(href, "my-roomname", doc)
-    window.wsProvider = wsProvider
-
-    wsProvider.on("status", (event) => {
-      console.log(event.status) // logs "connected" or "disconnected"
-    })
-    wsProvider.on("synced", (event) => {
-      console.log(`yjs synced`)
-      setProvider(wsProvider)
-      setDoc(doc)
-    })
-
-    return () => {
-      console.log(`DESTROY PROVIDER`)
-      wsProvider.destroy()
-    }
-  }, [])
+  const [document, changeDocument] = useDocument(queueHandle.url)
 
   return (
     <html lang="en">
@@ -84,10 +61,8 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Loading doc={syncedDoc}>
-          <YjsProvider trpc={trpc} provider={provider} doc={syncedDoc}>
+        <Loading doc={document}>
             <Outlet  />
-          </YjsProvider>
         </Loading>
         <ScrollRestoration />
         <Scripts />
