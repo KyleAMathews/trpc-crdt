b5755c0270ea69e07f195e841f3c04f5eeed2314
Kyle Mathews <mathews.kyle@gmail.com>
2023-11-10 09:38:31 -0800

    Fix client:generate in electric-sql example

--- examples/electric-sql/check-electric-is-running.js
+++ examples/electric-sql/check-electric-is-running.js
@@ -1,19 +1,24 @@
-const { fetchHostPortElectric, fetchHostProxyPortElectric } = require('./db/util.js')
-const { fetchConfiguredElectricPort, fetchConfiguredElectricProxyPort } = require('./util/util.js')
+const { fetchHostPortElectric, fetchHostProxyPortElectric } = require(
+  `./db/util.js`
+)
+const {
+  fetchConfiguredElectricPort,
+  fetchConfiguredElectricProxyPort,
+} = require(`./util/util.js`)
 
 async function checkElectricIsRunning() {
   const port = fetchHostPortElectric() // will raise an error if Electric is not running
-  
+
   // Check that the port on which Electric is running
   // is the same as the port to which the app will connect
   const configuredPort = await fetchConfiguredElectricPort()
-  
+
   if (configuredPort !== port) {
     console.error(
-      '\x1b[31m',
+      `\x1b[31m`,
       `Your application is configured to connect to Electric on port ${configuredPort} ` +
-      `but your instance of Electric is running on port ${port}`,
-      '\x1b[0m'
+        `but your instance of Electric is running on port ${port}`,
+      `\x1b[0m`
     )
     process.exit(1)
   }
@@ -23,10 +28,10 @@ async function checkElectricIsRunning() {
   const configuredProxyPort = await fetchConfiguredElectricProxyPort()
   if (configuredProxyPort !== proxyPort) {
     console.error(
-      '\x1b[31m',
+      `\x1b[31m`,
       `Your application is configured to connect to Electric's DB proxy on port ${configuredProxyPort} ` +
-      `but your instance of Electric is running the DB proxy on port ${proxyPort}`,
-      '\x1b[0m'
+        `but your instance of Electric is running the DB proxy on port ${proxyPort}`,
+      `\x1b[0m`
     )
   }
 }
